e5c89b7afea301b39dce25daa55af85cf2642dc1
Refaldy <refaldy.rizky22@gmail.com>
2023-07-08 21:24:12 +0700

    update

--- events/message.go
+++ events/message.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"wago/command"
@@ -15,6 +16,7 @@ import (
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 )
 
@@ -70,7 +72,12 @@ func MessageHandler(v *events.Message, client *whatsmeow.Client) {
 			if err != nil {
 				fmt.Println(err)
 			}
-			break
+			fmt.Println("Login Disini")
+			err = os.Remove(qr)
+			if err != nil {
+				fmt.Println(err)
+			}
+			return
 		}
 		switch v.Message.GetConversation() {
 		case "/login":
@@ -115,6 +122,11 @@ func MessageHandler(v *events.Message, client *whatsmeow.Client) {
 			if err != nil {
 				fmt.Println(err)
 			}
+			fmt.Println("Login Disana")
+			err = os.Remove(qr)
+			if err != nil {
+				fmt.Println(err)
+			}
 			break
 		}
 	}
@@ -135,8 +147,16 @@ func MessageHandler(v *events.Message, client *whatsmeow.Client) {
 			message := "🤷‍♂️"
 			client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{ExtendedTextMessage: &proto.ExtendedTextMessage{Text: &message, ContextInfo: &proto.ContextInfo{MentionedJid: participants}}})
 		}
+
 		args := strings.Split(commandMessage, " ")
 		switch args[0] {
+		case "reconnect":
+			client.Disconnect()
+			err := client.Connect()
+			if err != nil {
+				fmt.Errorf("Failed to connect: %v", err)
+			}
+			return
 		case "help":
 			command.HelpCommand(client, context.Background(), v.Info.Chat)
 			break
@@ -154,86 +174,142 @@ func MessageHandler(v *events.Message, client *whatsmeow.Client) {
 			message := "Success Logout..."
 			client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
 			client.Logout()
-			break
+			os.Remove(fmt.Sprintf("./whatsapp%s.db", v.Info.Chat.String()))
+			return
 		case "getlink":
 			if !strings.Contains(v.Info.Chat.String(), "@g.us") {
 				message := "Only In Group"
 				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
-				break
+				return
 			}
 			if len(args) > 1 {
+				go func() {}()
 				groups, _ := client.GetJoinedGroups()
 				remotes, _ := strconv.Atoi(args[1])
 				group, _ := client.GetGroupInviteLink(groups[remotes-1].JID, false)
 				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &group})
-
+				return
 			}
 			link, _ := client.GetGroupInviteLink(v.Info.Chat, false)
 			client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &link})
+			return
 			break
 		case "mygroup":
-			var nameGroup []string
-			groups, _ := client.GetJoinedGroups()
-			for _, v := range groups {
-				nameGroup = append(nameGroup, v.Name)
-			}
-			var builder strings.Builder
-			builder.WriteString("--LIST GROUP--\n\n")
-			for i, elem := range nameGroup {
-				builder.WriteString(fmt.Sprintf("%d.%s\n", i+1, elem))
-			}
+			go func() {
+				var nameGroup []string
+				groups, _ := client.GetJoinedGroups()
+				for _, v := range groups {
+					nameGroup = append(nameGroup, v.Name)
+				}
+				var builder strings.Builder
+				builder.WriteString("")
+				for i, elem := range nameGroup {
+					builder.WriteString(fmt.Sprintf("%d.%s\n", i+1, elem))
+				}
 
-			joinedString := builder.String()
-			client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &joinedString})
-			break
+				joinedString := builder.String()
+				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &joinedString})
+				return
+			}()
+			return
 		case "leaveremote":
 			if len(args) < 2 {
 				joinedString := "Please Give Me Argument"
 				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &joinedString})
+				return
 			}
-			groups, _ := client.GetJoinedGroups()
-			remotes, _ := strconv.Atoi(args[1])
-			message := fmt.Sprintf("Success Leave Group %s", groups[remotes-1].Name)
-			client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
-			client.LeaveGroup(groups[remotes-1].JID)
+			go func() {
+				groups, _ := client.GetJoinedGroups()
+				remotes, _ := strconv.Atoi(args[1])
+				message := fmt.Sprintf("Success Leave Group %s", groups[remotes-1].Name)
+				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
+				client.LeaveGroup(groups[remotes-1].JID)
+			}()
+			return
 		case "inforemote":
 			if len(args) < 2 {
 				group, _ := client.GetGroupInfo(v.Info.Chat)
 				message := fmt.Sprintf("INFO GROUP\n\nName: %s\nName Set By: %s\nCreated At: %s\nParticipant: %d\nOwner: %s", group.Name, group.NameSetBy.User, group.GroupCreated, len(group.Participants), group.OwnerJID.User)
 				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
+				return
 			} else {
-				groups, _ := client.GetJoinedGroups()
-				remotes, _ := strconv.Atoi(args[1])
-				group, _ := client.GetGroupInfo(groups[remotes-1].JID)
-				message := fmt.Sprintf("INFO GROUP\n\nName: %s\n\nName Set By: wa.me/%s\n\nCreated At: %s\n\nParticipant: %d\n\nOwner: wa.me/@%s", group.Name, group.NameSetBy.User, group.GroupCreated, len(group.Participants), group.OwnerJID.User)
-				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
+				go func() {
+					groups, _ := client.GetJoinedGroups()
+					remotes, _ := strconv.Atoi(args[1])
+					group, _ := client.GetGroupInfo(groups[remotes-1].JID)
+					message := fmt.Sprintf("INFO GROUP\n\nName: %s\n\nName Set By: wa.me/%s\n\nCreated At: %s\n\nParticipant: %d\n\nOwner: wa.me/@%s", group.Name, group.NameSetBy.User, group.GroupCreated, len(group.Participants), group.OwnerJID.User)
+					client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
+				}()
+				return
 			}
-			break
+			return
 		case "fakeremote":
 			if len(args) < 2 {
 				message := "no argument"
 				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
-				break
+				return
 			}
-			groups, _ := client.GetJoinedGroups()
-			remotes, _ := strconv.Atoi(args[1])
-			group, _ := client.GetGroupInfo(groups[remotes-1].JID)
-			var participants []string
-			for _, v := range group.Participants {
-				participants = append(participants, v.JID.String())
-			}
-			message := "🙂"
-			client.SendMessage(context.Background(), group.JID, &proto.Message{ExtendedTextMessage: &proto.ExtendedTextMessage{Text: &message, ContextInfo: &proto.ContextInfo{MentionedJid: participants}}})
+			go func() {
+				groups, _ := client.GetJoinedGroups()
+				remotes, _ := strconv.Atoi(args[1])
+				group, _ := client.GetGroupInfo(groups[remotes-1].JID)
+				var participants []string
+				for _, v := range group.Participants {
+					participants = append(participants, v.JID.String())
+				}
+				message := "🙂"
+				client.SendMessage(context.Background(), group.JID, &proto.Message{ExtendedTextMessage: &proto.ExtendedTextMessage{Text: &message, ContextInfo: &proto.ContextInfo{MentionedJid: participants}}})
+			}()
+			return
 		case "creategc":
 			if len(args) < 2 {
 				message := "no argument"
 				client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
-				break
+				return
 			}
 			newGroup, _ := client.CreateGroup(whatsmeow.ReqCreateGroup{Name: args[1]})
 			link, _ := client.GetGroupInviteLink(newGroup.JID, false)
 			client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &link})
-			break
+			return
+		case "spamtext":
+			go func() {
+				if len(args) < 2 {
+					message := "no argument"
+					client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
+					return
+				}
+				if args[1] == "" && args[2] == "" {
+					message := "no argument"
+					client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &message})
+					return
+				}
+				//apmas := strings.Split(v.Message.GetConversation(), "|")
+				// Mencocokkan teks setelah "/spamtext" dan sebelum "|"
+				re := regexp.MustCompile(`/spamtext\s*(.*?)\s*\|`)
+				match := re.FindStringSubmatch(v.Message.GetConversation())
+				var result string
+				if len(match) > 1 {
+					result = match[1]
+					fmt.Println(result) // Output: halo semuanya
+				}
+
+				// Mencocokkan teks setelah "|"
+				re = regexp.MustCompile(`\|\s*(.*?)\s*$`)
+				match = re.FindStringSubmatch(v.Message.GetConversation())
+
+				afterPipe := ""
+				if len(match) > 1 {
+					afterPipe = match[1]
+					fmt.Println(afterPipe) // Output: 10
+				}
+				jumlah, _ := strconv.Atoi(afterPipe)
+				for i := 1; i < int(jumlah); i++ {
+					go func() {
+						client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &result})
+					}()
+				}
+			}()
+
 		}
 	} else {
 		log.LogMe("RECEIVE MESSAGE", fmt.Sprintf("%s: %s -> %s\n", v.Info.Sender, v.Message.GetConversation(), v.Info.Chat))
@@ -272,6 +348,7 @@ func Login(id string) string {
 				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
 				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
 				err := qrcode.WriteFile(evt.Code, qrcode.Medium, 256, "qr"+id+".png")
+
 				if err != nil {
 					log.LogMe("LOGIN", "Gagal Login "+err.Error())
 				}
@@ -290,4 +367,23 @@ func Login(id string) string {
 	}
 
 	return ""
+}
+
+func parseJID(arg string) (types.JID, bool) {
+	if arg[0] == '+' {
+		arg = arg[1:]
+	}
+	if !strings.ContainsRune(arg, '@') {
+		return types.NewJID(arg, types.DefaultUserServer), true
+	} else {
+		recipient, err := types.ParseJID(arg)
+		if err != nil {
+			fmt.Errorf("Invalid JID %s: %v", arg, err)
+			return recipient, false
+		} else if recipient.User == "" {
+			fmt.Errorf("Invalid JID %s: no server specified", arg)
+			return recipient, false
+		}
+		return recipient, true
+	}
 }
